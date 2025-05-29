aa764e59ac6dbd3d3523b7b36154ee19c12ebcc4
Elbek <azimjonovelbek11@gmail.com>
2025-05-29 12:04:41 +0500

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\InventoryManagement-Django\InventoryManagement-Django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
